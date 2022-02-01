8e08bd2d76505bde04604e0be019b3fc57db952f
Jerad Jones <anvilringer4@yahoo.com>
2022-01-31 23:07:00 -0600

    Save to File
    Add the ability to save budget records to a text file

--- cBudget.c
+++ cBudget.c
@@ -1,16 +1,15 @@
 /*
  *
- * Name:    iBudget
+ * Name:    cBudget
  *
  * Purpose: Keep track of a simple budget in a text file.
  *
- *          Each budget record will have the following,
- *          where e = expense, and i = income.
+ *          Each budget record will have the following:
  *
  *          Date	Amount		Type		Description
  *          ____        ______          ____            ___________
  *
- *          mm/dd/yyyy	$xxxxxxx.yy	e/i	        Description of record
+ *          mm/dd/yyyy	$xxxxxxx.yy	Expense/Income  Description of record
  *
  *
  * Author:  jjones4
@@ -22,110 +21,148 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define MAX_RECORDS     100
-#define DATE_LEN        10
-#define AMOUNT_LEN      10
-#define TYPE_LEN        1
-#define DESCRIPTION_LEN 50
-#define NUM_TABS        6
+#define DATE_LEN        15
+#define AMOUNT_LEN      15
+#define TYPE_LEN        15
+#define DESCRIPTION_LEN 115
+#define FILE_NAME       "budget.txt"
 
 /* Function prototypes (declarations) */
-int read_str(char str[], int n, char budget_records[MAX_RECORDS][DATE_LEN \
-   + AMOUNT_LEN + TYPE_LEN + DESCRIPTION_LEN + NUM_TABS + 1], int num_records);
-void display_budget(char budget_records[MAX_RECORDS][DATE_LEN \
-   + AMOUNT_LEN + TYPE_LEN + DESCRIPTION_LEN + NUM_TABS + 1], int n);
+int read_str(char str[], int n);
+void display_budget(FILE* fp, char date_str[], char amount_str[], char type_str[], char desc_str[]);
+void save_data(FILE* fp, char date_str[], char amount_str[], char type_str[], char desc_str[]);
 
 int main(void)
 {
-   char records[MAX_RECORDS][DATE_LEN + AMOUNT_LEN + TYPE_LEN \
-      + DESCRIPTION_LEN + NUM_TABS + 1];
-   char date_str[DATE_LEN + 1], amount_str[AMOUNT_LEN + 1];
-   char type_str[TYPE_LEN + 1], desc_str[DESCRIPTION_LEN + 1];
-   int month, day, year, num_records = 0;
-   float amount;
+   FILE *fp;
+   int num_records = 0;
 
    while(1) {
+      char date_str[DATE_LEN + 2], amount_str[AMOUNT_LEN + 2];
+      char type_str[TYPE_LEN + 2], desc_str[DESCRIPTION_LEN + 2];
+      char clear_input[2];
 
-      if(num_records == MAX_RECORDS) {
+      if(num_records > 1000000) {
+
          printf("Too many records\n");
-         return (EXIT_FAILURE);
+         exit (EXIT_FAILURE);
       }
 
       printf("\n");
 
-      printf("Enter transaction date (q/Q to quit), mm/dd/yyyy: ");
-      read_str(date_str, DATE_LEN, &records[0], num_records);
-
-      printf("Enter transaction amount (q/Q to quit): ");
-      read_str(amount_str, AMOUNT_LEN, &records[0], num_records);
-
-      printf("Enter transaction type, e or i (q/Q to quit): ");
-      read_str(type_str, TYPE_LEN, &records[0], num_records);
-
-      printf("Enter transaction description (q/Q to quit): ");
-      read_str(desc_str, DESCRIPTION_LEN, &records[0], num_records);
-
-      /* Fill the records array with each part of the record */
-      int i = 0;
-      for(i = 0; i < DATE_LEN + AMOUNT_LEN + TYPE_LEN + DESCRIPTION_LEN + NUM_TABS;) {
-
-         int j = 0;
-
-         /* Fill the record array with the date */
-         while(j < DATE_LEN) {
-            records[num_records][i] = date_str[j];
-            i++;
-            j++;
-         }
-         records[num_records][i] = '\t';
-         i++;
-
-         /* Fill the record array with the amount */
-         j = 0;
-         while(amount_str[j] != '\0') {
-            records[num_records][i] = amount_str[j];
-            i++;
-            j++;
-         }
-         records[num_records][i] = '\t';
-         i++;
-         records[num_records][i] = '\t';
-         i++;
-
-         /* Fill the record array with the type */
-         j = 0;
-         while(j < TYPE_LEN) {
-            records[num_records][i] = type_str[j];
-            i++;
-            j++;
-         }
-         records[num_records][i] = '\t';
-         i++;
-         records[num_records][i] = '\t';
-         i++;
-
-         /* Fill the record array with the description */
-         j = 0;
-         while(j < DESCRIPTION_LEN) {
-            records[num_records][i] = desc_str[j];
-            i++;
-            j++;
-         }
-         records[num_records][i] = '\t';
-         i++;
-         records[num_records][i] = '\t';
-         i++;
+      /* Open file stream for budget data */
+      fp = fopen(FILE_NAME, "a+");
+      if(fp == NULL) {
+         printf("Can't open %s\n", FILE_NAME);
+         exit(EXIT_FAILURE);
       }
-      records[num_records][i] = '\0';
+
+      /* User menu */
+      printf("Type (1) to see your budget, (2) to add a record, or (3) to save and quit: ");
+
+      int c = getchar();
+
+      /* Display budget (option 1) */
+      if(c == '1') {
+
+         read_str(clear_input, 1);
+         
+         printf("\n");
+         printf("%-15s%-15s%-10s%-115s\n", "Date", "Amount", "Type", "Description");
+         printf("%-15s%-15s%-10s%-115s\n", "----", "------", "----", "-----------");
+         display_budget(fp, date_str, amount_str, type_str, desc_str);
+      }
+   
+      /* Enter budget record (option 2) */
+      else if(c == '2') {
+
+         read_str(clear_input, 1);
+
+         printf("Enter transaction date (mm/dd/yyyy): ");
+         read_str(date_str, DATE_LEN);
+         printf("Enter transaction amount: ");
+         read_str(amount_str, AMOUNT_LEN);
+         printf("Enter transaction type (Expense/Income): ");
+         read_str(type_str, TYPE_LEN);
+         printf("Enter transaction description: ");
+         read_str(desc_str, DESCRIPTION_LEN);
+
+         save_data(fp, date_str, amount_str, type_str, desc_str);
+      }
+
+      /* Exit program (option 3) */
+      else if(c == '3') {
+
+         exit(EXIT_SUCCESS);
+      }
+
+      /* Check for invalid option */
+      else {
+
+         read_str(clear_input, 1);
+
+         printf("You entered an invalid option. Try again.\n");
+         continue;
+      }
+
       num_records++;
    }
 
+   fclose(fp);
    return 0;
 }
 
-/* read_str stores input data into given array */
-int read_str(char s[], int n, char records[MAX_RECORDS][DATE_LEN \
-   + AMOUNT_LEN + TYPE_LEN + DESCRIPTION_LEN + NUM_TABS + 1], int num_records) {
+/* Save budget record into budget.txt */
+void save_data(FILE* fp, char date_str[], char amount_str[], char type_str[], char desc_str[]) {
+
+   /* Write record to budget.txt */
+   fprintf(fp, "%s", date_str);
+   fprintf(fp, "\n");
+   fprintf(fp, "%s", amount_str);
+   fprintf(fp, "\n");
+   fprintf(fp, "%s", type_str);
+   fprintf(fp, "\n");
+   fprintf(fp, "%s", desc_str);
+   fprintf(fp, "\n");
+}
+
+/* Display budget file entries on command line */
+void display_budget(FILE* fp, char date_str[], char amount_str[], char type_str[], char desc_str[]) {
+
+   int i = 1;
+   for(; ;) {
+
+      if(i % 4 == 1) {
+         if(fgets(date_str, DATE_LEN, fp) == NULL)
+            break;
+         date_str[strlen(date_str) - 1] = '\0';
+         i++;
+      }
+      if(i % 4 == 2) {
+         if(fgets(amount_str, AMOUNT_LEN, fp) == NULL)
+            break;
+         amount_str[strlen(amount_str) - 1] = '\0';
+         i++;
+      }
+      if(i % 4 == 3) {
+         if(fgets(type_str, TYPE_LEN, fp) == NULL)
+            break;
+         type_str[strlen(type_str) - 1] = '\0';
+         i++;
+      }
+      if(i % 4 == 0) {
+         if(fgets(desc_str, DESCRIPTION_LEN, fp) == NULL)
+            break;
+         desc_str[strlen(desc_str) - 1] = '\0';
+         i++;
+      }
+
+      printf("%-15s%-15s%-10s%-115s\n", date_str, amount_str, type_str, desc_str);
+   }
+}
+
+/* Read user input; store part of record into array */
+read_str(char s[], int n) {
 
    int c, i = 0;
 
@@ -136,40 +173,5 @@ int read_str(char s[], int n, char records[MAX_RECORDS][DATE_LEN \
    }
    s[i] = '\0';
 
-   /* Enter q or Q to exit */
-   if(strcmp(s, "Q") == 0 || strcmp(s, "q") == 0) {
-
-      /* If user quits immediately, do not print records */
-      if(num_records == 0) {
-         exit (EXIT_SUCCESS);
-      }
-
-      /* Print records when user quits */
-      display_budget(&records[0], num_records);
-
-      exit (EXIT_SUCCESS);
-   }
-
    return i;
-}
-
-/* Display budget */
-void display_budget(char records[MAX_RECORDS][DATE_LEN \
-   + AMOUNT_LEN + TYPE_LEN + DESCRIPTION_LEN + NUM_TABS + 1], int n) {
-
-   printf("\nDate\t\tAmount\t\tType\t\tDescription\n");
-   printf("----\t\t------\t\t----\t\t-----------\n");
-
-   int j, i = 0;
-   for(int i = 0; i < n; i++) {
-
-      j = 0;
-      while(records[i][j] != '\0') {
-         printf("%c", records[i][j]);
-          j++;
-      }
-      printf("\n");
-   }
-
-   printf("\n");
 }
